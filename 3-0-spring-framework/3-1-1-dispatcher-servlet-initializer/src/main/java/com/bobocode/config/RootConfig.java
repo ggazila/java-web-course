6117a664a3381d0073ec4131aa4e939b3d7e3c8c
serhiiyevtushok <ggazila@gmail.com>
2022-08-30 07:34:43 +0300

    3-1-1-dispatcher-servlet-initializer

--- 3-0-spring-framework/3-1-1-dispatcher-servlet-initializer/src/main/java/com/bobocode/config/RootConfig.java
+++ 3-0-spring-framework/3-1-1-dispatcher-servlet-initializer/src/main/java/com/bobocode/config/RootConfig.java
@@ -1,7 +1,8 @@
 package com.bobocode.config;
 
+import static org.springframework.context.annotation.ComponentScan.Filter;
+
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,9 +12,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  * (containing middle-tire services, datasource, etc.).
  * The configuration must exclude the web layer of the application.
  * <p>
- * todo: mark this class as config
- * todo: enable component scanning for all packages in "com.bobocode"
- * todo: ignore all web related config and beans (ignore @{@link Controller}, ignore {@link EnableWebMvc}) using exclude filter
  */
+@Configuration
+@ComponentScan(value = "com.bobocode", excludeFilters = { @Filter(Controller.class), @Filter(EnableWebMvc.class) })
+
 public class RootConfig {
 }
